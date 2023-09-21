80d3ef493543c2a8d896f5fbf6fb56d6e50b198a
kumankoishi.py <7iggif@naver.com>
2023-09-22 08:54:34 +0900

    draft: add shmup record type

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,22 @@ export interface NavNodeInfo {
   label: string;
   childNavNodeIds: NavNodeInfo['id'][];
 }
+
+export interface ShmupRecord {
+  id: string;
+  when: Date;
+  subjectId: string;
+  stage: string;
+  /**
+   * Why score should be string?
+   * In case of exceeding `Number.MAX_SAFE_INTEGER`, which equals to `9007199254740991`.
+   */
+  score: string;
+  byWhat: string;
+  comment: string;
+  thumbnailUrl: string;
+  originalImageUrl: string;
+  tweetUrl: string;
+  youtubeUrl?: string;
+  specialTags?: string[];
+}
