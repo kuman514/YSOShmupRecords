670b89859f89576dc3ee406e711df135756e29c9
kumankoishi.py <7iggif@naver.com>
2023-10-09 09:40:34 +0900

    refactor: add constants directory & add texts

---
+++ src/constants/texts.ts
@@ -0,0 +1,23 @@
+export const texts = {
+  intro: '개요',
+  criteria: '등록 기준',
+  records: '기록',
+  dodonpachi: '도돈파치 (1997)',
+  'dodonpachi-ashot': 'A-Shot',
+  'dodonpachi-alaser': 'A-Laser',
+  'dodonpachi-bshot': 'B-Shot',
+  'dodonpachi-blaser': 'B-Laser',
+  'dodonpachi-cshot': 'C-Shot',
+  'dodonpachi-claser': 'C-Laser',
+  galagaarrangement: '갤러그 어레인지먼트',
+  dariusgaiden: '다라이어스 외전',
+  // prettier-ignore
+  'dariusgaiden-zprime': 'Z\'존',
+  'dariusgaiden-v': 'V존',
+  'dariusgaiden-w': 'W존',
+  'dariusgaiden-x': 'X존',
+  'dariusgaiden-y': 'Y존',
+  'dariusgaiden-z': 'Z존',
+  // prettier-ignore
+  'dariusgaiden-vprime': 'V\'존',
+};
