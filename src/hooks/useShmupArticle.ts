ce8af03b859a4829dcf4e3a874db2e316310917b
kumankoishi.py <7iggif@naver.com>
2023-10-20 22:17:48 +0900

    refactor: make record hooks

---
+++ src/hooks/useShmupArticle.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+import { useShmupRecordStore } from '^/stores/shmup-record';
+import { ShmupRecord } from '^/types';
+import { getAPIURL } from '^/utils/api-url';
+
+export function useShmupArticle(pathName: string, currentRecordId?: string) {
+  const recordArticle = useShmupRecordStore((state) => state.recordArticle);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  const pathNameSplit = pathName.split('/').filter((path) => path.length > 1);
+
+  useEffect(() => {
+    if (currentRecordId === undefined) {
+      useShmupRecordStore.getState().setRecordArticle(undefined);
+      return;
+    }
+    (async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const response = await axios.get<ShmupRecord>(
+          getAPIURL(...pathNameSplit, currentRecordId)
+        );
+        useShmupRecordStore.getState().setRecordArticle({
+          ...response.data,
+          when: new Date(response.data.when),
+        });
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          useShmupRecordStore.getState().setRecordArticle(undefined);
+          setIsError(true);
+        }
+      }
+      setIsLoading(false);
+    })();
+  }, [currentRecordId]);
+
+  return {
+    recordArticle,
+    isLoading,
+    isError,
+  };
+}
---
+++ src/hooks/useShmupRecordIds.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import { useShmupRecordStore } from '^/stores/shmup-record';
+import { getAPIURL } from '^/utils/api-url';
+
+export function useShmupRecordIds(pathName: string) {
+  const recordIds = useShmupRecordStore((state) => state.recordIds);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  const pathNameSplit = pathName.split('/').filter((path) => path.length > 1);
+
+  useEffect(() => {
+    (async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const response = await axios.get<string[]>(
+          getAPIURL(...pathNameSplit, 'selection')
+        );
+        useShmupRecordStore.getState().setRecordIds(response.data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          useShmupRecordStore.getState().setRecordIds([]);
+          setIsError(true);
+        }
+      }
+      setIsLoading(false);
+    })();
+  }, [pathName]);
+
+  return {
+    recordIds,
+    isLoading,
+    isError,
+  };
+}
