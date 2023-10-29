4a3e2307b7d2452dbbb530953be7965de2690499
kumankoishi.py <7iggif@naver.com>
2023-10-30 00:17:04 +0900

    refactor: remove unnecessary style and comments

--- src/components/organisms/Article/index.tsx
+++ src/components/organisms/Article/index.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import { ArticleSummary } from '^/components/molecules/ArticleSummary';
 import { ArticleExtra } from '^/components/molecules/ArticleExtra';
 import { ShmupRecord } from '^/types';
-
-/**
- * @todo
- * Style Root if necessary. Otherwise, changing it into just article looks better.
- */
-const Root = styled.article``;
 
 interface Props {
   recordArticle: ShmupRecord;
@@ -17,12 +10,12 @@ interface Props {
 
 export function Article({ recordArticle }: Props) {
   return (
-    <Root>
+    <article>
       <ArticleSummary record={recordArticle} />
       <ArticleExtra
         comment={recordArticle.comment}
         youtubeUrl={recordArticle.youtubeUrl}
       />
-    </Root>
+    </article>
   );
 }
