f7b07f994ca724ac511594c8cc8ecca2ea02b887
kumankoishi <hadjadj0@gmail.com>
2023-12-10 23:34:18 +0900

    improvement: change article layout into notion style & limit main outlet width up to 720px (#25)
    * style: shrink thumbnail size
    
    * draft: remove nav route title from main & style for sub title
    
    * draft: style article summary & modify main component layout close to notion
    
    * style: show outlet in consistently same width
    
    * style: modify layout of article-extra area
    
    * test: modify what to test for youtube label area
    
    * style: weigh article title more
    
    * style: make article thumbnail not break its aspect ratio

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,25 +3,37 @@ import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
 
 import { Sidebar } from '^/components/organisms/Sidebar';
-import { NavRouteTitle } from '^/components/atoms/NavRouteTitle';
 import { rootNavNodes } from '^/constants/nav-node';
 
 const Root = styled.div`
   width: 100vw;
   height: 100vh;
-
   display: grid;
   grid-template-columns: auto 1fr;
+`;
+
+const OutletPositionHolder = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 72px;
+  padding-bottom: 72px;
+  overflow-y: scroll;
+`;
+
+const OutletContainer = styled.div`
+  width: 100%;
+  max-width: 720px;
 `;
 
 function Main() {
   return (
     <Root>
       <Sidebar rootNavNodes={rootNavNodes} />
-      <div>
-        <NavRouteTitle />
-        <Outlet />
-      </div>
+      <OutletPositionHolder>
+        <OutletContainer>
+          <Outlet />
+        </OutletContainer>
+      </OutletPositionHolder>
     </Root>
   );
 }
--- src/components/molecules/ArticleSummary/index.test.tsx
+++ src/components/molecules/ArticleSummary/index.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/react';
 
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import { ShmupRecord } from '^/types';
 import { convertDateToString } from '^/utils/date-to-string';
 
@@ -17,7 +18,6 @@ const testDataWithoutSpecialTags: ShmupRecord = {
   comment: '야스오 2-4 진출',
   thumbnailUrl: 'Thumbnail URL',
   originalImageUrl: 'Original Image URL',
-  tweetUrl: 'Tweet URL',
 };
 
 const testDataWithSpecialTags: ShmupRecord = {
@@ -30,8 +30,28 @@ describe('ArticleSummary', () => {
     cleanup();
   });
 
-  it('should have thumbnail, date title, stage, score, method, and tweet link', () => {
-    render(<ArticleSummary record={testDataWithoutSpecialTags} />);
+  it('should have thumbnail, date title, stage, score, method', () => {
+    const routes = [
+      {
+        path: '/test-sub1/test/koishi/hoshino',
+        element: <ArticleSummary record={testDataWithoutSpecialTags} />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/', '/test-sub1/test/koishi/hoshino'],
+      initialIndex: 1,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const renderResult1 = screen.getByText(/kuman514/i);
+    const renderResult2 = screen.getByText(/koishi/i);
+    const renderResult3 = screen.getByText(/hoshino/i);
+
+    expect(renderResult1).not.toStrictEqual(renderResult2);
+    expect(renderResult1).not.toStrictEqual(renderResult3);
+    expect(renderResult2).not.toStrictEqual(renderResult3);
 
     const thumbnail = screen.getByAltText(
       `${testDataWithoutSpecialTags.subjectId} ${convertDateToString(
@@ -55,13 +75,22 @@ describe('ArticleSummary', () => {
 
     const byWhat = screen.getByText(testDataWithoutSpecialTags.byWhat);
     expect(byWhat).not.toBeNull();
-
-    const tweetUrl = screen.getByText(testDataWithoutSpecialTags.tweetUrl);
-    expect(tweetUrl).not.toBeNull();
   });
 
   it('should show special tag area when the record have tags', () => {
-    render(<ArticleSummary record={testDataWithSpecialTags} />);
+    const routes = [
+      {
+        path: '/',
+        element: <ArticleSummary record={testDataWithSpecialTags} />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/'],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
 
     const specialTagTitle = screen.getByText(/특이사항/i);
     expect(specialTagTitle).not.toBeNull();
