7bf2188be0e39393c5945f7571e753a6d48ce227
kuman514 <hadjadj0@gmail.com>
2023-12-15 18:58:22 +0900

    refactor: remove unused current-record-id in store

--- src/stores/shmup-record.ts
+++ src/stores/shmup-record.ts
@@ -4,13 +4,11 @@ import { ShmupRecord } from '^/types';
 
 interface ShmupRecordState {
   recordIds: ShmupRecord['id'][];
-  currentRecordId?: ShmupRecord['id'];
   recordArticle?: ShmupRecord;
 }
 
 interface ShmupRecordAction {
   setRecordIds(newRecordIds: ShmupRecord['id'][]): void;
-  setCurrentRecordId(newCurrentRecordId?: ShmupRecord['id']): void;
   setRecordArticle(newRecordArticle?: ShmupRecord): void;
 }
 
@@ -22,11 +20,6 @@ export const useShmupRecordStore = create<ShmupRecordStore>((set) => ({
     set((store) => ({
       ...store,
       recordIds: newRecordIds,
-    })),
-  setCurrentRecordId: (newCurrentRecordId) =>
-    set((store) => ({
-      ...store,
-      currentRecordId: newCurrentRecordId,
     })),
   setRecordArticle: (newRecordArticle) =>
     set((store) => ({
