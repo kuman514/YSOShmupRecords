a0c2dbe64268425ed8bde007e13d0c7db9325e6c
kuman514 <hadjadj0@gmail.com>
2024-01-30 01:51:30 +0900

    feat: add & apply prev/next button for image-display

--- src/components/molecules/ImageDisplayModal/index.test.tsx
+++ src/components/molecules/ImageDisplayModal/index.test.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
-import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import {
+  act,
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+} from '@testing-library/react';
 
+import { Simulate } from 'react-dom/test-utils';
 import { ImageDisplayModal } from '.';
 
 describe('ImageDisplayModal', () => {
@@ -26,5 +33,75 @@ describe('ImageDisplayModal', () => {
       screen.getByAltText(/Record image in expanded contain size/i)
     );
     expect(mockFn).toBeCalledTimes(1);
+  });
+
+  it('should not show prev/next buttons while having only 1 image url', () => {
+    const { container } = render(
+      <ImageDisplayModal imageUrls={['']} onExit={() => {}} />
+    );
+
+    const prevImageButton = container.querySelector('#prev-image-button');
+    expect(prevImageButton).toBeNull();
+    const nextImageButton = container.querySelector('#next-image-button');
+    expect(nextImageButton).toBeNull();
+  });
+
+  it('should show prev/next buttons while having at least 2 image urls', () => {
+    const { container } = render(
+      <ImageDisplayModal imageUrls={['yasuo', 'koishi']} onExit={() => {}} />
+    );
+
+    const prevImageButton = container.querySelector('#prev-image-button');
+    expect(prevImageButton).not.toBeNull();
+    const nextImageButton = container.querySelector('#next-image-button');
+    expect(nextImageButton).not.toBeNull();
+  });
+
+  it('should have prev/next buttons operational', () => {
+    const { container } = render(
+      <ImageDisplayModal
+        imageUrls={['yasuo', 'koishi', 'maverick']}
+        onExit={() => {}}
+      />
+    );
+
+    act(() => {
+      const prevButton = container.querySelector('#prev-image-button');
+      if (!prevButton || !(prevButton instanceof HTMLElement)) {
+        throw Error('The prev button returned nullish');
+      }
+
+      Simulate.click(prevButton);
+    });
+
+    act(() => {
+      const prevButton = container.querySelector('#prev-image-button');
+      if (!prevButton || !(prevButton instanceof HTMLElement)) {
+        throw Error('The prev button returned nullish');
+      }
+
+      Simulate.click(prevButton);
+    });
+
+    const imageContainer1 = container.querySelector('img');
+    if (!imageContainer1 || !(imageContainer1 instanceof HTMLElement)) {
+      throw Error('The image element returned nullish');
+    }
+    expect(imageContainer1.src).toMatch(/koishi/i);
+
+    act(() => {
+      const nextButton = container.querySelector('#next-image-button');
+      if (!nextButton || !(nextButton instanceof HTMLElement)) {
+        throw Error('The next button returned nullish');
+      }
+
+      Simulate.click(nextButton);
+    });
+
+    const imageContainer2 = container.querySelector('img');
+    if (!imageContainer2 || !(imageContainer2 instanceof HTMLElement)) {
+      throw Error('The image element returned nullish');
+    }
+    expect(imageContainer2.src).toMatch(/maverick/i);
   });
 });
--- src/components/molecules/ImageDisplayModal/index.tsx
+++ src/components/molecules/ImageDisplayModal/index.tsx
@@ -14,21 +14,75 @@ const CloseButtonWrapper = styled.div`
   padding: 16px;
 `;
 
+const NextPrevButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  pointer-events: none;
+
+  & * {
+    pointer-events: fill;
+  }
+`;
+
+const NextPrevButton = styled.div`
+  width: 30%;
+  max-width: 300px;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: var(--white-color);
+
+  font-size: 24px;
+  font-weight: 700;
+`;
+
 interface Props {
   imageUrls: string[];
   onExit(): void;
 }
 
 export function ImageDisplayModal({ imageUrls, onExit }: Props) {
-  /**
-   * @todo
-   * Make going to prev/next image
-   */
-  const [index] = useState<number>(0);
+  const [index, setIndex] = useState<number>(0);
+
+  const renderNextPrevButtonWrapper =
+    imageUrls.length > 1 ? (
+      <NextPrevButtonWrapper>
+        <NextPrevButton
+          id="prev-image-button"
+          onClick={() => {
+            const newIndex = (index - 1 + imageUrls.length) % imageUrls.length;
+            setIndex(newIndex);
+          }}
+        >
+          {'<'}
+        </NextPrevButton>
+        <NextPrevButton
+          id="next-image-button"
+          onClick={() => {
+            const newIndex = (index + 1) % imageUrls.length;
+            setIndex(newIndex);
+          }}
+        >
+          {'>'}
+        </NextPrevButton>
+      </NextPrevButtonWrapper>
+    ) : null;
 
   return (
     <Overlay>
       <ImageZoomAndMoveController imageUrl={imageUrls[index]} />
+      {renderNextPrevButtonWrapper}
       <CloseButtonWrapper>
         <Button
           type={ButtonType.CLEAR}
