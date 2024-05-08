7615ae9ba2642962db65b5bfb8d5dfdace5406eb
kumankoishi <hadjadj0@gmail.com>
2024-05-09 00:16:52 +0900

    refactor: split router from entry point (#71)

---
+++ src/route.tsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import Main from '^/Main';
+import { CriteriaPage } from '^/pages/CriteriaPage';
+import { ErrorPage } from '^/pages/ErrorPage';
+import { IntroPage } from '^/pages/IntroPage';
+import { LandingPage } from '^/pages/LandingPage';
+import { RecordListPage } from '^/pages/RecordListPage';
+import { RecordPage } from '^/pages/RecordPage';
+import { TerminologyPage } from '^/pages/TerminologyPage';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    children: [
+      {
+        path: '',
+        element: <LandingPage />,
+      },
+      {
+        path: 'intro',
+        element: <IntroPage />,
+      },
+      {
+        path: 'criteria',
+        element: <CriteriaPage />,
+      },
+      {
+        path: 'terminology',
+        element: <TerminologyPage />,
+      },
+      {
+        path: 'records',
+        children: [
+          {
+            path: ':typeId',
+            children: [
+              {
+                path: '',
+                element: <RecordListPage />,
+              },
+              {
+                path: ':recordDateId',
+                element: <RecordPage />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
