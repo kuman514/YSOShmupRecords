494eba95c031326c04cd2fc371acb5b4a0b2d060
kumankoishi.py <7iggif@naver.com>
2024-08-19 13:31:45 +0900

    improvement: reduce record nav-nodes to show primary types & fill out the space with other contents

--- src/constants/nav-node.ts
+++ src/constants/nav-node.ts
@@ -44,83 +44,20 @@ export const rootNavNodes: NavNodeInfo[] = [
     linkTo: '/records',
     childNavNodes: [
       {
-        id: 'dodonpachi',
-        childNavNodes: [
-          {
-            id: 'dodonpachi-ashot',
-            linkTo: '/records/dodonpachi-ashot',
-          },
-          {
-            id: 'dodonpachi-alaser',
-            linkTo: '/records/dodonpachi-alaser',
-          },
-          {
-            id: 'dodonpachi-bshot',
-            linkTo: '/records/dodonpachi-bshot',
-          },
-          {
-            id: 'dodonpachi-blaser',
-            linkTo: '/records/dodonpachi-blaser',
-          },
-          {
-            id: 'dodonpachi-cshot',
-            linkTo: '/records/dodonpachi-cshot',
-          },
-          {
-            id: 'dodonpachi-claser',
-            linkTo: '/records/dodonpachi-claser',
-          },
-        ],
+        id: 'dodonpachi-cshot',
+        linkTo: '/records/dodonpachi-cshot',
       },
       {
         id: 'galagaarrangement',
         linkTo: '/records/galagaarrangement',
       },
       {
-        id: 'dariusgaiden',
-        childNavNodes: [
-          {
-            id: 'dariusgaiden-zprime',
-            linkTo: '/records/dariusgaiden-zprime',
-          },
-          {
-            id: 'dariusgaiden-v',
-            linkTo: '/records/dariusgaiden-v',
-          },
-          {
-            id: 'dariusgaiden-w',
-            linkTo: '/records/dariusgaiden-w',
-          },
-          {
-            id: 'dariusgaiden-x',
-            linkTo: '/records/dariusgaiden-x',
-          },
-          {
-            id: 'dariusgaiden-y',
-            linkTo: '/records/dariusgaiden-y',
-          },
-          {
-            id: 'dariusgaiden-z',
-            linkTo: '/records/dariusgaiden-z',
-          },
-          {
-            id: 'dariusgaiden-vprime',
-            linkTo: '/records/dariusgaiden-vprime',
-          },
-        ],
+        id: 'ketsui-b',
+        linkTo: '/records/ketsui-b',
       },
       {
-        id: 'ketsui',
-        childNavNodes: [
-          {
-            id: 'ketsui-a',
-            linkTo: '/records/ketsui-a',
-          },
-          {
-            id: 'ketsui-b',
-            linkTo: '/records/ketsui-b',
-          },
-        ],
+        id: 'inthehunt',
+        linkTo: '/records/inthehunt',
       },
     ],
   },
