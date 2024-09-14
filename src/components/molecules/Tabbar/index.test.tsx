e5981c9896b382877528ddc94d364789124bc870
kuman514 <hadjadj0@gmail.com>
2024-09-14 17:21:36 +0900

    test: add minimum requirements for tabbar

---
+++ src/components/molecules/Tabbar/index.test.tsx
@@ -0,0 +1,16 @@
+import { cleanup } from '@testing-library/react';
+import { beforeEach, describe, it } from 'vitest';
+
+describe('Tabbar', () => {
+  beforeEach(() => {
+    cleanup();
+  });
+
+  it('should show tabs just as provided', () => {});
+
+  it('should have tabs that are all clickable', () => {});
+
+  it('should show first tab active at first', () => {});
+
+  it('should switch active tab when click a tab', () => {});
+});
